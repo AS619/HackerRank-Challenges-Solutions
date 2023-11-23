b34ff7635861129a864e296d66b81f06e08ebe65
Ayesha Sandeepani <137033424+AS619@users.noreply.github.com>
2023-11-23 13:52:55 +0530

    Update 01_string_split_and_join.py

--- Python/01_string_split_and_join.py
+++ Python/01_string_split_and_join.py
@@ -2,21 +2,22 @@
 
 This code defines a function split_and_join that takes a string, splits it into a list of words using spaces as separators, and then joins the words using hyphens. 
 The modified string is then returned.
-"""
+
+     
 
 def split_and_join(line):
+  
+    a = line.split(" ")    # Split the line into a list of words using spaces as separators
 
-     a = line.split(" ")
-
-    b = "-".join(a)
+    b = "-".join(a)        # Join the words using hyphens
 
     return(b)
 
 
 if __name__ == '__main__':
 
-    line = input()
+    line = input()           # Input a line of text
 
-    result = split_and_join(line)
+    result = split_and_join(line)    # Call the function 
 
-    print(result)
+    print(result)                    # print the result
